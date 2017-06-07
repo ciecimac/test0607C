b299092b869310eb7a30c97537d2743463e85bf0
ciecimac <ciecclm@imnu.edu.cn>
2017-06-07 09:06:33 +0800

    重新下载
    提交

---
+++ test0607C/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  test0607C
+//
+//  Created by clm on 2017/6/7.
+//  Copyright © 2017年 clm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("今天高考!\n");
+    return 0;
+}
